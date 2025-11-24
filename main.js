e799718aa9c2860d6a5a0e2405d1607be91a3152
Leo <leoquan09@gmail.com>
2025-11-24 08:47:20 -0800

    Finished the project
    Completed the game flow of the rock paper scissors game and added a
    button that starts the game.

--- main.js
+++ main.js
@@ -1,1 +1,34 @@
-console.log('Hello world')
+let getRob = () => {
+    let rand = Math.floor(Math.random() * 3);
+    let robot = ''
+    if (rand === 0) {
+        robot = 'rock';
+    } else if (rand === 1) {
+        robot = 'paper';
+    } else {
+        robot = 'scissors';
+    }
+    return robot;
+}
+
+let game = () => {
+    let human = prompt('Rock, paper, or scissors: ');
+    let rob = getRob();
+
+    //determine winner
+    if (human == 'rock' && rob == 'scissors') {
+        console.log('You won!');
+    } else if (human == 'scissors' && rob == 'paper') {
+        console.log('Player 1 won!');
+    }else if (human == 'paper' && rob == 'rock') {
+        console.log('You won!');
+    } else if (human === rob) {
+        console.log('Its a tie!');
+    } else {
+        console.log('You lost...');
+    }
+}
+
+
+let start = document.getElementById('start');
+start.addEventListener('mousedown', game);
